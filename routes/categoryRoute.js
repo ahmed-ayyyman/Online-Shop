5678ee9d5f02a24501d37a29a89697c80a3d215f
Ahmed Ayman <ahmed.eldawdy22@gmail.com>
2026-03-08 14:14:11 +0200

    feat: add image processing for category updates

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -34,6 +34,11 @@ router
 router
   .route("/:id")
   .get(getCategoryValidator, getCategory)
-  .put(updateCategoryValidator, updateCategory)
+  .put(
+    createCategoryImage,
+    resizeImage,
+    updateCategoryValidator,
+    updateCategory,
+  )
   .delete(deleteCategoryValidator, deleteCategory);
 module.exports = router;
